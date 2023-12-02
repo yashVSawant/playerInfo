643639132cc570dfb2986da8e0c1430e4996e1e9
yashVSawant <yashsawant23910@gmail.com>
2023-12-02 15:13:09 +0530

    made few chenges

--- public/js/main.js
+++ public/js/main.js
@@ -28,15 +28,63 @@ search.addEventListener('click',async()=>{
     const name = player.value;
     console.log(name);
     try{
-        // const getPlayer = await axios.get(`http://localhost:3000/getInfo?name=${name}`)
-        // console.log(getPlayer);
-        showInfoPage();
+        const getPlayer = await axios.get(`http://localhost:3000/getInfo?name=${name}`)
+        console.log(getPlayer.data);
+        showInfoPage(getPlayer.data);
     }catch(err){
         console.log(err)
     }
 })
 
-function showInfoPage(){
+function showInfoPage(data){
  const showInfoDiv = document.getElementById('showInfo');
  showInfoDiv.style.display='inline'
+ imageAndInfo(data.photoUrl,data.name,data.birthdate);
+ personalInfo(data.numOfMatch,data.score,data.fifties,data.centuries,data.average,data.wickets,data.career)
+}
+
+function imageAndInfo(url,playerName,date){
+    const getDiv = document.getElementById('imageAndInfo');
+    const image = document.createElement('img');
+    const name = document.createElement('h3');
+    const birthdate = document.createElement('p');
+    const age =  new Date().getFullYear();
+
+    image.src=url;
+    name.innerText=playerName;
+    birthdate.innerText=date;
+
+    getDiv.appendChild(image);
+    getDiv.appendChild(name);
+    getDiv.appendChild(birthdate);
+
+}
+
+function personalInfo(M,R,F,C,A,W,cR){
+    const getDiv = document.getElementById('personalInfo');
+    const careerDiv = document.getElementById('careerInfo');
+
+    const matches = document.createElement('p');
+    const runs = document.createElement('p');
+    const fifties = document.createElement('p');
+    const centuries = document.createElement('p');
+    const average = document.createElement('p');
+    const wickets = document.createElement('p');
+    const career = document.createElement('p');
+    matches.innerText=`matches :${M}`;
+    runs.innerText=`runs :${R}`;
+    fifties.innerText=`fifties :${F}`;
+    centuries.innerText=`centuries :${C}`;
+    average.innerText=`average :${A}`;
+    wickets.innerText=`wickets :${W}`;
+    career.innerText =cR
+
+    getDiv.appendChild(matches);
+    getDiv.appendChild(runs);
+    getDiv.appendChild(fifties);
+    getDiv.appendChild(centuries);
+    getDiv.appendChild(average);
+    getDiv.appendChild(wickets);
+
+    careerDiv.appendChild(career);
 }
